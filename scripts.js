98804f0938f8f5c08e96e29cfb2d58eda452f171
AnuBis7Star <enriquenascutiualin1@gmail.com>
2025-09-02 17:36:05 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+const container = document.querySelector("#songList");
+container.addEventListener("click", (event) => {
+  const deleteBtn = event.target.closest('[data-action="delete"]');
+  if (!deleteBtn) return; // not a delete click
+  // now deleteBtn is *the exact button* you clicked
+  deleteBtn.closest('.row').remove();
+
+  renumber();
+});
+
+const dialog = document.querySelector(".dialog-backdrop");
+
+const new_song = document.getElementById("add-song");
+new_song.addEventListener("click", () => {
+  dialog.style.display = "flex";
+});
+
+const dialog_exit = document.getElementById("dialog-cancel");
+dialog_exit.addEventListener("click", () => {
+  dialog.style.display = "none";
+})
+
+// Renumber the list of songs
+
+function renumber() {
+  const rows = document.querySelectorAll("#songList .row");
+  
+  rows.forEach((row, index) => {
+    const numberCell = row.querySelector(".n");
+    numberCell.textContent = index + 1; // index starts at 0, so add 1
+  });
+}
+
+
